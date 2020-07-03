51136948239ec9dcacd20f137b8b410261c0fd24
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-03 08:38:52 +0800

    Add command-line flag for options

--- src/main.hs
+++ src/main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards, TypeApplications, TupleSections #-}
+{-# LANGUAGE RecordWildCards, TypeApplications, TupleSections, ApplicativeDo #-}
 module Main where
 
 import Data.Attoparsec.Text (parseOnly)
@@ -11,7 +11,7 @@ import ScottCheck.Engine
 
 import Control.Monad.Writer
 import qualified Data.Map as M
-import Data.SBV
+import Data.SBV hiding (options)
 import Data.SBV.Internals (SMTModel(..), CV(..), CVal(..))
 import qualified Data.SBV.Maybe as SBV
 import Debug.Trace
@@ -20,12 +20,39 @@ import Data.List (stripPrefix, sortBy)
 import Data.Ord (comparing)
 import Data.Either
 import Data.Array
+import Options.Applicative
+
+data Options = Options
+    { filePath :: FilePath
+    , inputLength :: Int
+    }
+
+options :: Parser Options
+options = do
+    inputLength <- option auto $ mconcat
+        [ long "input-length"
+        , short 'n'
+        , metavar "LENGTH"
+        , help "How many commands to input"
+        ]
+    filePath <- strArgument $ mconcat
+        [ metavar "FILENAME"
+        ]
+    pure Options{..}
+
+optionsInfo = info (options <**> helper) $ mconcat
+    [ fullDesc
+    , header "ScottCheck"
+    , progDesc "SMT based verification of Scott Adams adventure games"
+    ]
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering
+    Options{..} <- execParser optionsInfo
 
-    s <- T.readFile "t0.sao"
+    -- hSetBuffering stdout NoBuffering
+
+    s <- T.readFile filePath
     let Right theGame = parseOnly game s
     -- print theGame
 
@@ -36,7 +63,7 @@ main = do
             finished
 
     SatResult r <- sat $ do
-        cmds <- forM (take 4 [0..]) $ \i -> do
+        cmds <- forM [1..inputLength] $ \i -> do
             let word name = do
                     var <- free $ printf "%s@%d" name (i :: Int)
                     constrain $ 0 .<= var .&& var .< literal (gameDictSize theGame)
@@ -47,6 +74,7 @@ main = do
         return $ SBV.fromMaybe sFalse $ fst $ play cmds
 
     let solution = case r of
+            Unsatisfiable _ _ -> error "Cannot solve"
             Satisfiable _config SMTModel{..} -> zip
                                                 (map (resolve gameVerbsRaw . snd) . sortBy (comparing fst) $ verbs)
                                                 (map (resolve gameNounsRaw . snd) . sortBy (comparing fst) $ nouns)
@@ -64,25 +92,4 @@ main = do
                 , Right . read @Int <$> stripPrefix "noun@" s
                 ]
 
-    print solution
-
-    let inputs =
-            [ ("south", "")
-            , ("get", "any")
-            , ("north", "")
-            , ("drop", "coin")
-            ]
-        cmds = [ (literal v, literal n)
-               | (w1, w2) <- inputs, Just (v, n) <- return $ parseInput theGame w1 w2
-               ]
-
-    let cmds =
-            [ (literal 1, literal 2)
-            , (literal 10, literal 7)
-            , (literal 1, literal 1)
-            , (literal 18, literal 7)
-            ]
-
-    let (endResult, msgs) = play cmds
-    mapM_ print msgs
-    print endResult
+    forM_ solution $ \(w1, w2) -> printf "> %s %s\n" w1 w2
