8c835dc20eb6fd163d68fd61460afae4544e47fd
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-12 17:26:12 +0800

    simplify

--- src/main.hs
+++ src/main.hs
@@ -5,7 +5,6 @@ import ScottCheck.Engine
 import Data.SBV hiding (options, solve)
 import Data.SBV.Control
 import qualified Data.Array as A
-import Control.Monad.IO.Class
 
 main :: IO ()
 main = do
@@ -20,19 +19,9 @@ main = do
             push 1
             (finished, s) <- return $ step theGame (verb1, noun1) s
             constrain finished
-            cs <- checkSat
-            liftIO $ print cs
+            ensureSat
 
-            pop 1
-            verb2 <- genWord
-            noun2 <- genWord
-            push 1
-            (finished, s) <- return $ step theGame (verb2, noun2) s
-            constrain finished
-            cs <- checkSat
-            liftIO $ print cs
-
-            mapM getValue [verb1, noun1, verb2, noun2]
+            mapM getValue [verb1, noun1]
     mapM_ print cmds
   where
     genWord = do
