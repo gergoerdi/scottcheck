c8d8f7d23d70638bd283c261c5d46ebbffedd9aa
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-12 17:22:17 +0800

    simplify

--- src/main.hs
+++ src/main.hs
@@ -4,49 +4,40 @@ import ScottCheck.Engine
 
 import Data.SBV hiding (options, solve)
 import Data.SBV.Control
-import qualified Data.SBV.Tuple as SBV
 import qualified Data.Array as A
-
-
-loopState :: (SymVal i) => (Int -> Query (SBV i)) -> s -> (SBV i -> s -> (SBool, s)) -> Query [i]
-loopState genCmd s0 step = go 1 s0 []
-  where
-    go i s cmds = do
-        io $ putStrLn $ "Searching at depth: " ++ show i
-
-        cmd <- genCmd i
-        let cmds' = cmds ++ [cmd]
-
-        push 1
-        let (finished, s') = step cmd s
-        constrain finished
-        cs <- checkSat
-
-        case cs of
-            Unk -> error $ "Solver said Unknown, depth: " ++ show i
-            Unsat -> do
-                pop 1
-                go (i+1) s' cmds'
-            Sat -> mapM getValue cmds'
+import Control.Monad.IO.Class
 
 solve :: Game -> IO ()
 solve theGame = do
     cmds <- runSMT $ do
         arr <- newArray "items" Nothing
-        query $ loopState genInput (initState theGame arr) $ \cmd -> do
-            let (verb, noun) = SBV.untuple cmd
-            step theGame (verb, noun)
+        query $ do
+            s <- return $ initState theGame arr
 
+            verb1 <- genWord
+            noun1 <- genWord
+            push 1
+            (finished, s) <- return $ step theGame (verb1, noun1) s
+            constrain finished
+            cs <- checkSat
+            liftIO $ print cs
+
+            pop 1
+            verb2 <- genWord
+            noun2 <- genWord
+            push 1
+            (finished, s) <- return $ step theGame (verb2, noun2) s
+            constrain finished
+            cs <- checkSat
+            liftIO $ print cs
+
+            mapM getValue [verb1, noun1, verb2, noun2]
     mapM_ print cmds
   where
     genWord = do
         word <- freshVar_
         constrain $ 0 .<= word .&& word .< literal (gameDictSize theGame)
         return word
-    genInput i = do
-        verb <- genWord
-        noun <- genWord
-        return $ SBV.tuple (verb, noun)
 
 main :: IO ()
 main = do
