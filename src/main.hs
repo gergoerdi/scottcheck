fb96d220aca437539c48d6b2a11d7608bb53cc3a
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-12 17:52:52 +0800

    simplify

--- src/main.hs
+++ src/main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module Main where
 
 import Data.Int
@@ -6,29 +7,19 @@ import Data.SBV.Control
 
 type SArr = SFunArray
 
-type S = SArr Int16 Int16
-
-step :: [Maybe Int16] -> (SInt16, SInt16) -> S -> SBool
-step items (verb, noun) locs = readArray locs' 0 .== 1
-  where
-    locs' = ite (verb .== 0) builtin_get $
-            ite (verb .== 1) builtin_drop $
-            locs
-
-    builtin_get = writeArray locs item $ let loc = readArray locs item in ite (loc .== 1) 255 loc
-    builtin_drop = writeArray locs item $ let loc = readArray locs item in ite (loc .== 255) 1 loc
-
-    item = ite (noun .== 7) 0 (-1)
-
 main :: IO ()
-main = do
-    let theGame = [Just 7]
-    cmds <- runSMT $ do
-        arr <- newArray "items" Nothing
-        query $ do
-            verb <- freshVar "verb"
-            noun <- freshVar "noun"
-            constrain $ step theGame (verb, noun) (writeArray arr 0 255)
-            ensureSat
-            mapM getValue [verb, noun]
-    mapM_ print cmds
+main = runSMT $ do
+    arr <- newArray_ @SArr @Int16 @Int16 Nothing
+    query $ do
+        x <- freshVar_ @Int16
+        let ys = writeArray arr 0 255
+        constrain $
+          let ys' = ite (x .== 0) ys1 $ ite (x .== 1) ys2 ys
+
+              ys1 = writeArray ys idx $ ite (y .== 1) 255 y
+              ys2 = writeArray ys idx $ ite (y .== 255) 1 y
+
+              idx = ite (x .== 1) 0 (-1)
+              y = readArray ys idx
+          in readArray ys' 0 .== 1
+        ensureSat
