3bfc77a500a3da63e497ee68a6a4daf1a759ebd1
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-29 14:05:27 +0800

    Game data parser

---
+++ src/main.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE RecordWildCards, TypeApplications, TupleSections, ApplicativeDo #-}
+module Main where
+
+import Data.Attoparsec.Text (parseOnly)
+import qualified Data.Text.IO as T
+import System.IO
+import System.Exit
+
+import ScottCheck.GameData
+import ScottCheck.Parse
+-- import ScottCheck.Engine
+-- import ScottCheck.Utils
+
+-- import qualified Data.Map as M
+-- import Data.SBV hiding (options, solve)
+-- import Data.SBV.Control
+-- import Data.SBV.Internals (SMTModel(..), CV(..), CVal(..))
+-- import qualified Data.SBV.Maybe as SBV
+-- import qualified Data.SBV.Tuple as SBV
+-- import Debug.Trace
+-- import Text.Printf
+-- import Data.List (stripPrefix, sortBy)
+-- import Data.Ord (comparing)
+-- import Data.Either
+-- import Data.Array
+import Options.Applicative
+-- import Control.Monad
+-- import Control.Monad.State
+-- import Control.Monad.IO.Class
+
+data Options = Options
+    { filePath :: FilePath
+    }
+
+options :: Parser Options
+options = do
+    filePath <- strArgument $ mconcat
+        [ metavar "FILENAME"
+        ]
+    pure Options{..}
+
+optionsInfo = info (options <**> helper) $ mconcat
+    [ fullDesc
+    , header "ScottCheck"
+    , progDesc "SMT based verification of Scott Adams adventure games"
+    ]
+
+play :: Game -> IO ()
+play = print
+
+main :: IO ()
+main = do
+    Options{..} <- execParser optionsInfo
+
+    hSetBuffering stdout NoBuffering
+
+    s <- T.readFile filePath
+    let Right theGame = parseOnly game s
+    -- print theGame
+
+    play theGame
